63d033b394c3a232b20dde1d895b066b8df3ab17
Lukas Horak <lukasbhorak@gmail.com>
2019-07-05 15:05:34 -0400

    split tests into separate js script

--- MusicLibrary.js
+++ MusicLibrary.js
@@ -41,44 +41,4 @@ class Library {
   }
 }
 
-
-
-// Tests to prove I did the thing:
-const test = new Library({
-  name: "My Library",
-  creator: "The Awesomest Dude Ever"
-});
-
-test.createPlaylist( "My First Playlist" );
-
-const testTrack = new Track({
-  title: "Familiar Drugs",
-  artist: "Alexisonfire",
-  album: "Familiar Drugs",
-  rating: 5,
-  trackLength: 258
-});
-
-const otherTestTrack = new Track({
-  title: "Old Crows",
-  artist: "Alexisonfire",
-  album: "Old Crows/Young Cardinals",
-  rating: 4,
-  trackLength:  257
-})
-
-test.addTrackToPlaylist({
-  track: testTrack,
-  playlistName: "My first Playlist"
-});
-
-test.addTrackToPlaylist({
-  track: otherTestTrack,
-  playlistName: "my first playlist"
-})
-
-test.getPlaylist("my first playlist", (playlist) => {
-  console.log("Playlist Rating: ", playlist.overallRating);
-  console.log("Total playlist runtime (in seconds): ", playlist.totalDuration)
-  console.log("Total playlist runtime (formatted to hh:mm:ss): ", playlist.formattedDuration)
-})
+module.exports = Library;
